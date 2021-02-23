0d382b0c81d4f03b4482c19986de62f7371974f5
unknown <surajaghav23@gmail.com>
2021-02-23 18:28:59 +0530

    Updates

---
+++ precache-manifest.edffe1d567bbb2ff5e6956faff4067ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627515eea233c247f2acdf5181fff22d",
+    "url": "/React-Profile-Finder/index.html"
+  },
+  {
+    "revision": "046d9d7a457bf70683d4",
+    "url": "/React-Profile-Finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/React-Profile-Finder/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "046d9d7a457bf70683d4",
+    "url": "/React-Profile-Finder/static/js/main.5b767094.chunk.js"
+  },
+  {
+    "revision": "cefa4bb78c4eb96184b1",
+    "url": "/React-Profile-Finder/static/js/runtime~main.08e0650e.js"
+  }
+]);
